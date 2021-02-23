35d9c3357a2a51d5276aba4fa6a2d21b55d440d3
Gaffer Fitch <gaffer.fitch@gmail.com>
2021-02-23 00:15:01 -0500

    Added limit (#12)

--- lib/relations_pymysql.py
+++ lib/relations_pymysql.py
@@ -289,6 +289,14 @@ class Source(relations.Source):
                 order_bys.append(field[1:] if field[0] == "+" else f"{field[1:]} DESC")
             query.add(order_bys=order_bys)
 
+        if model._limit is not None:
+            if model._offset:
+                query.add(limits="%s, %s")
+                values.extend([model._offset, model._limit])
+            else:
+                query.add(limits="%s")
+                values.append(model._limit)
+
         self.record_retrieve(model._record, query, values)
 
         cursor.execute(query.get(), values)
